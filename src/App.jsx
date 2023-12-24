2743c437859dd356d9479717a1f677309cffa4cd
Jia Xin Tang Zhi <72444963+jiatangzhi@users.noreply.github.com>
2023-12-24 15:54:21 +0100

    updated hero and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
 import styles from './style';
+import {
+  Navbar,
+  Hero,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Stats,
+  Testimonials
+} from "./components";
+
+
 
 const App = () => {
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>Hero</div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <Hero/>
+    </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      Stats
-      Business
-      Billing
-      CardDeal
-      Testimonials
-      Clients
-      CTA
-      Footer
+      Stats Business Billing CardDeal Testimonials Clients CTA Footer
     </div>
   </div>;
 };
